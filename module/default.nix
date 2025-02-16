0adff53b32624909fefb6c82d8670aef64faa199
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-16 21:05:27 +0100

    refactor: format

--- module/default.nix
+++ module/default.nix
@@ -1,36 +1,38 @@
 { pkgs, systemInformation, ... }:
 
 {
-  home-manager.users."${systemInformation.userName}" = { ... }: {
-    programs.helix = {
-      enable = true;
+  home-manager.users."${systemInformation.userName}" =
+    { ... }:
+    {
+      programs.helix = {
+        enable = true;
 
-      settings = {
-        editor = {
-          line-number = "relative";
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
+        settings = {
+          editor = {
+            line-number = "relative";
+            cursor-shape = {
+              insert = "bar";
+              normal = "block";
+              select = "underline";
+            };
+            lsp.display-inlay-hints = true;
+            auto-format = true;
           };
-          lsp.display-inlay-hints = true;
-          auto-format = true;
         };
+
+        languages.language = [
+          {
+            name = "nix";
+            formatter.command = "nixfmt";
+            auto-format = true;
+          }
+        ];
       };
 
-      languages.language = [
-        {
-          name = "nix";
-          formatter.command = "nixfmt";
-          auto-format = true;
-        }
+      home.packages = with pkgs; [
+        nil
+        nixd
+        nixfmt-rfc-style
       ];
     };
-
-    home.packages = with pkgs; [
-      nil
-      nixd
-      nixfmt-rfc-style
-    ];
-  };
 }
